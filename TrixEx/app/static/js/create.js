8337852f5fd22ecb13546f6d6d5a6edf1f5871b0
Garett.Janulewicz <janulewiczgarett@gmail.com>
2024-01-11 17:40:41 -1000

    create page screen

--- TrixEx/app/static/js/create.js
+++ TrixEx/app/static/js/create.js
@@ -1,19 +1,19 @@
-
-// Initialize CodeMirror with different variable names
-var htmleditor = CodeMirror.fromTextArea(document.getElementById("htmleditor"), {
+// Text Editor
+var htmlEditor = CodeMirror.fromTextArea(document.getElementById("htmlEditor"), {
     lineNumbers: true,
     mode: "htmlmixed", // Set the programming language mode
     htmlMode: true,
     theme: 'moxer',
+    onChange: updatePreview, // Use CodeMirror's onChange event
 });
 
-var csseditor = CodeMirror.fromTextArea(document.getElementById("csseditor"), {
+var cssEditor = CodeMirror.fromTextArea(document.getElementById("cssEditor"), {
     lineNumbers: true,
     mode: "css", // Set the programming language mode
     theme: 'moxer',
 });
 
-var jseditor = CodeMirror.fromTextArea(document.getElementById("jseditor"), {
+var jsEditor = CodeMirror.fromTextArea(document.getElementById("jsEditor"), {
     lineNumbers: true,
     mode: "javascript", // Set the programming language mode
     theme: 'moxer',
@@ -21,6 +21,31 @@ var jseditor = CodeMirror.fromTextArea(document.getElementById("jseditor"), {
 
 // Set the height for each editor
 var newHeight = "calc(100vh / 3)"; // Adjust the height as needed
-htmleditor.setSize(null, newHeight);
-csseditor.setSize(null, newHeight);
-jseditor.setSize(null, newHeight);
+htmlEditor.setSize(null, newHeight);
+cssEditor.setSize(null, newHeight);
+jsEditor.setSize(null, newHeight);
+
+// Attach onChange event after the editors are created
+htmlEditor.on("change", updatePreview);
+cssEditor.on("change", updatePreview);
+jsEditor.on("change", updatePreview);
+
+// Update screen preview
+function updatePreview() {
+    const htmlCode = htmlEditor.getValue();
+    const cssCode = cssEditor.getValue();
+    const jsCode = jsEditor.getValue();
+    var iframe = document.getElementById('screen');
+
+    // Clear existing content
+    iframe.innerHTML = "";
+
+
+    // Write HTML and CSS to the iframe document
+    var doc = iframe.contentDocument || iframe.contentWindow.document;
+    doc.open();
+    doc.write("<body>" + htmlCode + "</body>");
+    doc.write("<style>" + cssCode + "</style>");
+    doc.write("<script>" + jsCode + "</" + "script>");
+    doc.close();
+}
